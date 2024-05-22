2a3e6a36d083c36eca74e2addb95f37443a94705
Guillaume Georges <guillaume.georges.fr@gmail.com>
2024-05-22 16:49:43 +0700

    Update refresh_materialized_table.sql

--- refresh_materialized_table.sql
+++ refresh_materialized_table.sql
@@ -1,22 +1,16 @@
-DELIMITER $$
+START TRANSACTION;
 
-CREATE EVENT IF NOT EXISTS refresh_materialized_table
-ON SCHEDULE EVERY 1 DAY
-DO
-BEGIN
-    START TRANSACTION;
-    CREATE TEMPORARY TABLE IF NOT EXISTS temp_SG_product_full_info_materialized LIKE SG_product_full_info_materialized;
-    TRUNCATE TABLE temp_SG_product_full_info_materialized;
-    INSERT INTO temp_SG_product_full_info_materialized SELECT * FROM SG_only_product_full_info;
-    SET @count = (SELECT COUNT(*) FROM temp_SG_product_full_info_materialized);
-    IF @count > 0 THEN
-      TRUNCATE TABLE SG_product_full_info_materialized;
-      INSERT INTO SG_product_full_info_materialized SELECT * FROM temp_SG_product_full_info_materialized;
-      COMMIT;
-    ELSE
-      ROLLBACK;
-    END IF;
-    DROP TEMPORARY TABLE IF EXISTS temp_SG_product_full_info_materialized;
-END$$
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_SG_product_full_info_materialized LIKE SG_product_full_info_materialized;
+TRUNCATE TABLE temp_SG_product_full_info_materialized;
+INSERT INTO temp_SG_product_full_info_materialized SELECT * FROM SG_only_product_full_info;
 
-DELIMITER ;
+SET @count = (SELECT COUNT(*) FROM temp_SG_product_full_info_materialized);
+IF @count > 0 THEN
+    TRUNCATE TABLE SG_product_full_info_materialized;
+    INSERT INTO SG_product_full_info_materialized SELECT * FROM temp_SG_product_full_info_materialized;
+    COMMIT;
+ELSE
+    ROLLBACK;
+END IF;
+
+DROP TEMPORARY TABLE IF EXISTS temp_SG_product_full_info_materialized;
