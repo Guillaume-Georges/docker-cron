cfa8dec947fff029589e6cdad5e80d66af2d2246
Guillaume Georges <guillaume.georges.fr@gmail.com>
2024-05-22 16:47:58 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
-
+#!/bin/bash
+# Print environment variables to a file
 env >> /etc/environment
 
-# execute CMD
-echo "$@"
-exec "$@"
+# Ensure permissions for cron directory
+chmod 0644 /etc/cron.d/hello-cron
+
+# Apply cron job
+crontab /etc/cron.d/hello-cron
+
+# Start the cron service in the foreground
+echo "Starting cron..."
+cron -f -L 15
